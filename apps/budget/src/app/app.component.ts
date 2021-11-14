aa49658df007b9a8bba1b0fb6bd01b400d40cc57
Umair Hafeez <umairhafeez.ca@gmail.com>
2021-11-14 15:49:14 -0500

    initial commit

---
+++ apps/budget/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'limitless-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less'],
+})
+export class AppComponent {
+  title = 'budget';
+}
